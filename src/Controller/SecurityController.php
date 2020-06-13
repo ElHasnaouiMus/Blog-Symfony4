75cd7e694758014e5524a8ba28c14d23d14503bd
Mustapha El Hasnaoui <elhasnaouimus@gmail.com>
2020-06-13 15:38:24 +0100

    add Logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -45,4 +45,10 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig');
     }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout(){}
+    
 }
